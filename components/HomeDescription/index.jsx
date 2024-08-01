6696f89c75851c80f060f22cb5ec183b6aebf3a6
Michael Li <michaelli.fr@gmail.com>
2024-08-01 01:07:12 -0400

    added description background lines

--- components/HomeDescription/index.jsx
+++ components/HomeDescription/index.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
+import { useRef, useEffect, useState} from 'react';
+import { useInView, motion } from 'framer-motion';
 import styles from './styles.module.scss';
+import {slideUp, opacity} from "./animation";
+import AppearsInView from "#/AppearInView";
+import CurvedLine from "#/CurvedLine";
 
 export default function index() {
-  const phraseOne = "A drive to";
-  const rotateWords = ["Create", "Optimize", "Repeat"]
-  const phraseTwo = "My comprehensive understanding of Frontend, Algorithms, and mechanical systems gives me a holistic understanding of a product development process.";
+  const bg = useRef(null)
+  const [numEl, setNumEl] = useState(0);
   
-  
+  const phrase = "My comprehensive understanding of Front-end, Algorithms, and Mechanical Systems gives me a unique understanding of the product development process.";
+
+  useEffect(() => {
+    setNumEl(Math.floor(bg.current.clientHeight / 100))
+  })
+
   return (
     <div className={styles.backgroundContainer}>
-      <div className={styles.contentContainer}>
-        <div className={styles.sectionOne}>
-          <div className={styles.phraseContainer}>
+      <div ref={bg} className={styles.lbg}>
             {
-              phraseOne.split(" ").map((word, index) => {
-                return <span className={styles.mask}><span>{word}</span></span>
+              [...Array(numEl).keys()].map((num, index) => {
+                return <CurvedLine key={index} className={styles.curvedLine} stroke='rgba(184, 184, 184, 0.2)' style={{top: `calc(200px + ${index * 150}px)`}}/>
               })
             }
-            <div className={styles.videoOne}></div>
+        <div className={styles.contentContainer}>
+          <h1 className={styles.title}>Featured Works</h1>
+          <div className={styles.workDisplay1}>
+            <div className={styles.videoContainer}><video src="/ki_rotation.webm" autoPlay={"autoplay"} muted loop /></div>
+            <p>[001] Flight Path Planning for Unmanned Drone</p>
           </div>
-        </div>
-        <div className={styles.sectionTwo}>
-
+          <div className={styles.workDisplay2}>
+            <div className={styles.videoContainer}><video src="/battery_rotation.webm" autoPlay={"autoplay"} muted loop /></div>
+            <p>[002] 600V Custom Li-ion Battery Pack</p>
+          </div>
+          <div className={styles.workDisplay3}>
+            <div className={styles.videoContainer}><video src="/ki_fe_demo.webm" autoPlay={"autoplay"} muted loop /></div>
+            <p>[003] Flight Path Planning for Unmanned Drone</p>
+          </div>
+          <AppearsInView className={styles.descriptionText}>{phrase}</AppearsInView>
         </div>
       </div>
     </div>
--- components/Landing/Footer/index.jsx
+++ components/Landing/Footer/index.jsx
@@ -5,30 +5,16 @@ import FlipText from '#/FlipText';
 
 export default function index() {
 
-  const socials = [
-    {
-      title: "read.cv",
-      href: "https://read.cv/michaelli",
-    },
-  ]
-
   return (
     <div className={styles.footer}>
-      {
-        socials.map( (data, index) => {
-          return <Magnetic>
-                  <div className={styles.links}>
-                    <Arrow size={18}/>
-                    <a 
-                    key={index} 
-                    href={data.href} 
-                    className={styles.link}>
-                    <FlipText alterText={data.title}>{data.title}</FlipText>
-                    </a>
-                  </div>
-                </Magnetic>
-        })
-      }
+      <Magnetic>
+        <div className={styles.links}>
+          <Arrow size={18}/>
+          <a href="https://read.cv/michaelli" className={styles.link}>
+            <FlipText alterText="read.cv">read.cv</FlipText>
+          </a>
+        </div>
+      </Magnetic>
     </div>
   )
 }
